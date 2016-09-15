f9a7dd71934c8479eb574c406b22a6f3dc1d62d0
wounds1 <mok15asd@yahoo.com>
2016-09-15 14:44:01 +0300

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -1,29 +1,56 @@
 do 
-
-local function DevPoint(msg, matches) 
-if is_sudo(msg) then 
-        local wounds = "انته المطور مالتي 🕵🔧".."\n".."- 🆔 أيـديــك : "..msg.from.id.."\n".."🏧- أســمـك : "..msg.from.first_name.."\n".."🚸- مــعــرفــك  : @"..msg.from.username.."\n".."©- أســم ألـمـجـمـوعـة   "..msg.to.title --Dev @wounds1 
-        return reply_msg(msg.id, wounds, ok_cb, false) 
-     end 
+local function bakury(msg,matches) 
+ if is_sudo(msg) then 
+   local sudo = "عزيز موقعك ومعلوماتك هنا 👇🏌"..msg.from.first_name.."\n" 
+   .."🆔👤ايديك :("..msg.from.id..")\n" 
+   .."🆔📡ايدي الكروب :("..msg.to.id..")\n" 
+   .."⛽️معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   .."📞رقمك :("..(msg.from.phone or " لا يوجد")..")\n" 
+   .."موقعك👤: انته مطور مالتي 🕵👋 " 
+return reply_msg(msg.id, sudo, ok_cb, true) 
+ end 
+ if is_owner(msg) then 
+ local owner = "عزيزي موقعك ومعلوماتك هنا 👇🏌"..msg.from.first_name.."\n" 
+   .."🆔👤ايديك :("..msg.from.id..")\n" 
+   .."🆔📡ايدي الكروب :("..msg.to.id..")\n" 
+   .."⛽️معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   .."📞رقمك :("..(msg.from.phone or " لا يوجد ")..")\n" 
+   .."👤موقعك :انته مدير المجموعه 🚨 👥 »" 
+return reply_msg(msg.id, owner, ok_cb, true) 
+ end 
+ if is_admin1(msg) then 
+local admin1 = "عزيزي موقعك ومعلوماتك هنا👷👇🏼" 
+   .."🆔👤ايديك :("..msg.from.id..")\n" 
+   .."🆔📡ايدي الكروب :("..msg.to.id..")\n" 
+   .."⛽️معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   .."📞رقمك :("..(msg.from.phone or " لايوجد ")..")\n" 
+   .."👷موقعك :انته ادراي 🙋» " 
+return reply_msg(msg.id, admin1, ok_cb, true) 
+end 
 if is_momod(msg) then 
-        local wounds = "أنـت أدمــن ألـكـروب 🛳⚓️".."\n".."🆔- أيــديــك  : "..msg.from.id.."\n".."🏧- أســمـك : "..msg.from.first_name.."\n".."🚸- مــعــرفــك : @"..msg.from.username.."\n".."©- أســم ألـمـجـمـوعـة "..msg.to.title 
-        return reply_msg(msg.id, wounds, ok_cb, false) 
-     end 
-if not is_momod(msg) then 
-        local wounds = "انت عضووو  😹💙".."\n".."🆔- أيـديــك : "..msg.from.id.."\n".."🏧- أســمـك : "..msg.from.first_name.."\n".."🚸- مــعــرفــك : @"..msg.from.username.."\n".."©- أسـم ألـمـجـمـوعـة "..msg.to.title 
-        return reply_msg(msg.id, wounds, ok_cb, false) 
-     end 
-if is_owner(msg) then 
-        local wounds = "أنـت مــديــر ألـكـروب 🤖🤘🏼".."\n".."🆔- أيــديــك : "..msg.from.id.."\n".."🏧- أســمـك : "..msg.from.first_name.."\n".."🚸- مــعــرفــك  : @"..msg.from.username.."\n".."©- أســم ألـمـجـمـوعـة   "..msg.to.title 
-        return reply_msg(msg.id, wounds, ok_cb, false) 
-     end 
-     end 
+      local admin = "عزيزي موقعك ومعلوماتك هنا 👮👇🏼" "..msg.from.first_name.."\n" 
+   .."🆔👤ايديك :("..msg.from.id..")\n" 
+   .."🆔📡ايدي الكروب :("..msg.to.id..")\n" 
+   .."⛽️معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   .."📞رقمك :("..(msg.from.phone or " لا يوجد ")..")\n" 
+   .."👮موقعك :انته ادمن المحترم 🎩" 
+return reply_msg(msg.id, admin, ok_cb, true) 
+ end 
+ if not is_momod(msg) then 
+      local member = "عزيزي موقعك ومعلوماتك هنا🕵👇🏼" "..msg.from.first_name.."\n" 
+   .."🆔👤ايديك :("..msg.from.id..")\n" 
+   .."🆔📡ايدي الكروب :("..msg.to.id..")\n" 
+   .."📌معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   .."📞رقمك :("..(msg.from.phone or "لايوجد " )..")\n" 
+   .."🚸موقعك :انته دايح قصدي عضو 😹💔» " 
+return reply_msg(msg.id, member, ok_cb, true) 
+ end 
+end 
 
 return { 
   patterns = { 
-       "^موقعي$", 
+       "^(موقعي)$", 
   }, 
-  run = DevPoint, 
+  run = marslo, 
 } 
-
-end 
+end
