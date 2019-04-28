670b3b3ad8bbbbc3832ffb3d0829a186d4ef3511
troybot <49770000+troybot@users.noreply.github.com>
2019-04-28 13:27:27 +0100

    Update devpoint.lua

--- bot/devpoint.lua
+++ bot/devpoint.lua
@@ -275,9 +275,10 @@ function create_config( )
     "echo",
     "send",
     "linkpv",
-    "sudolist"
+    "sudolist",
+    "games",
     },
-    sudo_users = {124406196},--Sudo users
+    sudo_users = {776008296},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[DevPoint v1
 An advanced administration bot based on TG-CLI written in Lua
